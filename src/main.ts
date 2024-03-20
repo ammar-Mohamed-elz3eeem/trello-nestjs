e66ae28030f8f644c57e63ee01832bcb2966f879
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2024-03-20 04:44:34 +0200

    edit port assigned to app

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser(process.env.COOKIE_SECRET));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
