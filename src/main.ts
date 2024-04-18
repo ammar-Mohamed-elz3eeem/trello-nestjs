dab62f041dd334d3554145f01eff57437986890c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2024-04-18 18:27:59 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:*',
+  'https://e-commerce-nestjs-1ad58a9d5c9f.herokuapp.com/',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: whitelist,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.use(cookieParser(process.env.COOKIE_SECRET));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
